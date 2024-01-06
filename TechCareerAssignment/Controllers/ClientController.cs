90e7c9e7c5e9b4a35008c4600b2a68d4c0b12e98
Selim Şahin <selimsahin.sns@gmail.com>
2024-01-06 15:24:38 +0300

    Added ClientController and CompanyController

---
+++ TechCareerAssignment/Controllers/ClientController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TechCareerAssignment.Data;
+using TechCareerAssignment.Models;
+
+namespace TechCareerAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ClientController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/Client
+        [HttpGet]
+        public ActionResult<IEnumerable<Client>> GetClients()
+        {
+            var clients = _dbContext.Clients;
+            return Ok(clients);
+        }
+
+        // GET: api/Client/5
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Client> GetClient(int id)
+        {
+            var client = _dbContext.Clients.Find(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(client);
+        }
+
+        // POST: api/Client
+        [HttpPost]
+        public ActionResult<Client> CreateClient([FromBody] Client newClient)
+        {
+            if (newClient == null)
+            {
+                return BadRequest("Client data is null.");
+            }
+
+            _dbContext.Clients.Add(newClient);
+            _dbContext.SaveChanges();
+
+            return CreatedAtAction("GetClient", new { id = newClient.Id }, newClient);
+        }
+
+        // PUT: api/Client/5
+        [HttpPut]
+        [Route("{id}")]
+        public ActionResult<Client> UpdateClient(int id, [FromBody] Client updatedClient)
+        {
+            if (updatedClient == null)
+            {
+                return BadRequest("Client data is null.");
+            }
+
+            var client = _dbContext.Clients.Find(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            client.Name = updatedClient.Name;
+            client.Surname = updatedClient.Surname;
+            client.BirthDate = updatedClient.BirthDate;
+            client.Address = updatedClient.Address;
+            client.EMail = updatedClient.EMail;
+            client.CompanyId = updatedClient.CompanyId;
+
+            _dbContext.SaveChanges();
+
+            return Ok(client);
+        }
+
+        // DELETE: api/Client/5
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult<Client> DeleteClient(int id)
+        {
+            var client = _dbContext.Clients.Find(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Clients.Remove(client);
+            _dbContext.SaveChanges();
+
+            return Ok(client);
+        }
+    }
+}
---
+++ TechCareerAssignment/Controllers/CompanyController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TechCareerAssignment.Data;
+using TechCareerAssignment.Models;
+
+namespace TechCareerAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CompanyController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/Company
+        [HttpGet]
+        public ActionResult<IEnumerable<Company>> GetCompanies()
+        {
+            var companies = _dbContext.Companies;
+            return Ok(companies);
+        }
+
+        // GET: api/Company/5
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Company> GetCompany(int id)
+        {
+            var company = _dbContext.Companies.Find(id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(company);
+        }
+
+        // POST: api/Company
+        [HttpPost]
+        public ActionResult<Company> CreateCompany([FromBody] Company newCompany)
+        {
+            if (newCompany == null)
+            {
+                return BadRequest("Company data is null.");
+            }
+
+            _dbContext.Companies.Add(newCompany);
+            _dbContext.SaveChanges();
+
+            return CreatedAtAction("GetCompany", new { id = newCompany.Id }, newCompany);
+        }
+
+        // PUT: api/Company/5
+        [HttpPut]
+        [Route("{id}")]
+        public ActionResult<Company> UpdateCompany(int id, [FromBody] Company updatedCompany)
+        {
+            if (updatedCompany == null)
+            {
+                return BadRequest("Company data is null.");
+            }
+
+            var company = _dbContext.Companies.Find(id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            company.Name = updatedCompany.Name;
+            company.Address = updatedCompany.Address;
+
+            _dbContext.SaveChanges();
+
+            return Ok(company);
+        }
+
+        // DELETE: api/Company/5
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult<Company> DeleteCompany(int id)
+        {
+            var company = _dbContext.Companies.Find(id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Companies.Remove(company);
+            _dbContext.SaveChanges();
+
+            return Ok(company);
+        }
+    }
+}
